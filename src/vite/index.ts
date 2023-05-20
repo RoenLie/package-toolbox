ebd34c8fac5f9b5001ae977a4050bc31e35f43a9
RoenLie <RoenLie@users.noreply.github.com>
2023-05-20 20:18:03 +0000

    Increment version through github action.

---
+++ src/vite/index.ts
@@ -0,0 +1,7 @@
+/* eslint-disable simple-import-sort/exports */
+/* eslint-disable max-len */
+/* auto generated */
+export type { AutoImportPluginProps, AutoImportLoadProps } from './vite-ce-auto-import.js';
+export { componentAutoImportLoad, componentAutoImporter } from './vite-ce-auto-import.js';
+export { libConfig } from './vite-lib-config.js';
+export { viteCopy } from './vite-plugin-copy.js';
